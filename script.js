edb6f6975aea9a900d9f08093444816fe3d4506a
kennja05 <jacobkenny05@gmail.com>
2020-08-10 11:36:26 -0400

    add update

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ ctx.fillText('A', 50, 60);
 const data = ctx.getImageData(0, 0, 100, 100) 
 
 class Particle {
+
     constructor(x, y){
         this.x = x;
         this.y = y; 
@@ -30,20 +31,35 @@ class Particle {
         this.baseY = this.y
         this.density = (Math.random() * 30)+ 1
     }
+
     draw(){
-        ctx.fillStyle ='white'
+        ctx.fillStyle ='red'
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
         ctx.closePath()
         ctx.fill()
+    }
+    
+    update(){
+        let dx = mouse.x - this.x
+        let dy = mouse.y - this.y
+        let distance = Math.sqrt(dx * dx + dy * dy)
+        if (distance < 100){
+            this.size = 30;
+        } else {
+            this.size = 3
+        }
     }
 
 }
 
 function init(){
     particleArray = []
-    particleArray.push(new Particle(50, 50))
-    particleArray.push(new Particle(80, 50))
+    for (let i = 0; i < 1000; i++){
+        let x = Math.random() * canvas.width
+        let y = Math.random() * canvas.height
+        particleArray.push(new Particle(x, y))
+    }
 }
 init()
 
@@ -51,6 +67,7 @@ function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < particleArray.length; i++){
         particleArray[i].draw()
+        particleArray[i].update()
     }
     requestAnimationFrame(animate)
 }
